83e5c3dea57c7cbb731cb8a06b54f445da13d577
Bawantha <bawanthapasqual@gmail.com>
2025-07-02 21:27:22 +0530

    add z index in navbar

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="min-h-screen relative">
       <div className="absolute inset-0 bg-[url('/Richmond_1st_flag.jpg')] bg-cover bg-center bg-no-repeat opacity-40"></div>
-      <div className="relative z-10">
+      <div className="relative z-20">
         <Navbar />
       </div>
       <div className="relative z-10">
